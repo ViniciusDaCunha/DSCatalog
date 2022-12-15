6bc6514e2c4579390e9be4dd959310b86d619115
Vinicius da Cunha <dacunhavini@gmail.com>
2022-12-15 12:16:16 -0300

    Update Category

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscatalog.resources;
 
-
 import java.net.URI;
 import java.util.List;
 
@@ -9,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,35 +22,42 @@ import com.devsuperior.dscatalog.services.CategoryService;
 //rota REST (requisições web) do recurso
 public class CategoryResource {
 	/**
-	 * Recebe requisições da API(a API ajudará a comunicar o que você quer ao sistema 
-	 * para que ele entenda e realize o que foi solicitado).Pense nas APIs como um mediador 
-	 * entre os usuários ou clientes e os recursos ou serviços web que eles querem obter.
+	 * Recebe requisições da API(a API ajudará a comunicar o que você quer ao
+	 * sistema para que ele entenda e realize o que foi solicitado).Pense nas APIs
+	 * como um mediador entre os usuários ou clientes e os recursos ou serviços web
+	 * que eles querem obter.
 	 */
-	
+
 	@Autowired
-	//injeta dependencia com a camada service que chama repository
+	// injeta dependencia com a camada service que chama repository
 	private CategoryService service;
-	
+
 	@GetMapping
-	//Anotação para mapear solicitações HTTP
-	public ResponseEntity<List<CategoryDTO>> findAll(){
+	// Anotação para mapear solicitações HTTP
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	//Anotação para mapear solicitações HTTP
-	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+	// Anotação para mapear solicitações HTTP
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-	
+
 	@PostMapping
-	//inserir novo recurso
-	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+	// inserir novo recurso
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
 		dto = service.insert(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(dto.getId()).toUri();	
-	return ResponseEntity.created(uri).body(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	// atualizar um recurso
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 	}
 }
